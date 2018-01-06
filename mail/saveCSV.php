a0a4e4ff04b31b1c5c4ebbb44cc5cf62f6af80e6
Tincho <alvarezdmartinn@gmail.com>
2018-01-05 22:19:13 -0300

    Delete if, add variable

--- mail/saveCSV.php
+++ mail/saveCSV.php
@@ -2,13 +2,8 @@
 // $saveToCSV = pathfile
 if ($saveToCSV !== false){
 // Save to CSV file
-
-if (isset($_POST["formulario"])) {
-// Prepara conexión DB
-      include("open_connexion.php");
-
       $file = fopen($saveToCSV, "w");
-      fputcsv($file, array('ID','Nombre','Email','Lugar de inicio','CHAPELCO','SAN MARTIN','ASPEN','SANKT','VAIL','BAQUERIA','NISEKO','OTRO DESTINO','Fecha aproximada de inicio','Cantidad de dias','Fecha desde','Fechas hasta','Fechas flexibles','Cantidad de personas','Cantidad de niños','Edad promedio de niños','Prefencia hotel','Alquiler equipos','Tomar clases','ESQUIAR','FAMILIA','VIDA NOCTURNA','RELAX/SPA','SHOPPING','OTRO OBJETIVO','Observaciones','Fecha envio formulario'),';');
+      fputcsv($file, array('ID','Nombre','Email','Lugar de inicio','CHAPELCO','ASPEN','LAS LEÑAS','GRANDVALIRA','BAQUERIA','NISEKO','OTRO DESTINO','Fecha desde','Fechas hasta','Fechas flexibles','Cantidad de personas','Cantidad de niños','Edad promedio de niños','Prefencia hotel','Alquiler equipos','Tomar clases','ESQUIAR','FAMILIA','VIDA NOCTURNA','RELAX/SPA','SHOPPING','OTRO OBJETIVO','Observaciones','Fecha envio formulario'),';');
       $tabla_db = "formulario";
       $query = "SELECT * FROM $tabla_db ORDER BY `id` ASC";
       $result_query = mysqli_query($conexion_db, $query);
@@ -17,6 +12,5 @@ if (isset($_POST["formulario"])) {
           fputcsv($file, $fila);
       }
       fclose($file);
-      include("close_connexion.php");
-}
+      $URL_DESCARGA = $saveToCSV.'/consultas.csv';
 }
