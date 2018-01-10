7c794d2f5e196615dd442b0a73e0fcb30d0f9b5f
lucianoar <luciano@littlebox.com.ar>
2018-01-10 18:18:05 -0300

    Use PDO instead of mysqli, and prepared statements
    Fix indentation also

--- mail/mail.php
+++ mail/mail.php
@@ -1,123 +1,163 @@
 <?php
 
-  // $to = "reservas@skirience.com";
-  $to = "reservas@skirience.com";
+// $to = "reservas@skirience.com";
+$to = "reservas@skirience.com";
 
-  // Prepara conexión DB
-  require_once("open_conexion.php");
+// Prepara conexión DB
+require_once("open_conexion.php");
 
-  if (isset($_POST["consulta"])) {
-      // Setea variables de consulta
-      $nombre = $_POST["name"];
-      $email = $_POST["email"];
-      $mensaje = $_POST["mensaje"];
+if (isset($_POST["consulta"])) {
+  // Setea variables de consulta
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $mensaje = $_POST["mensaje"];
 
-      //Prepara mail
-      $subject = "Consulta desde Web";
-      $message = "Nombre: " . $nombre;
-      $message .= "<br><br>Email: " . $email;
-      $message .= "<br><br>Mensaje: " . $mensaje;
-      $headers = "MIME-Version: 1.0" . "\r\n";
-      $headers .= "Content-type: text/html; charset=utf-8" . "\r\n";
-      $headers .= "From: Formulario Web <web@skirience.com>". "\r\n";
-      $headers .= "Reply-To: ".$nombre." <".$email.">". "\r\n";
+  //Prepara mail
+  $subject = "Consulta desde Web";
+  $message = "Nombre: " . $name;
+  $message .= "<br><br>Email: " . $email;
+  $message .= "<br><br>Mensaje: " . $mensaje;
+  $headers = "MIME-Version: 1.0" . "\r\n";
+  $headers .= "Content-type: text/html; charset=utf-8" . "\r\n";
+  $headers .= "From: Formulario Web <web@skirience.com>" . "\r\n";
+  $headers .= "Reply-To: " . $name . " <" . $email . ">" . "\r\n";
 
-  } elseif (isset($_POST["formulario"])) {
-      // Preparo variables
-      $nombre = $_POST['name'];
-      $email = $_POST['email'];
-      $lugar_inicio_viaje = $_POST['inicio_viaje'];
-      empty($_POST['Chapelco,San_Martin_de_los_Andes']) ? $destino_chapelco = 'NO' : $destino_chapelco = 'SI';
-      empty($_POST['Aspen_Snowmas,Colorado']) ? $destino_aspen_colorado = 'NO' : $destino_aspen_colorado = 'SI';
-      empty($_POST['Baquera_Beret,España']) ? $destino_baqueria_españa = 'NO' : $destino_baqueria_españa = 'SI';
-      empty($_POST['Niseko,Japon']) ? $destino_niseko_japon = 'NO' : $destino_niseko_japon = 'SI';
-      empty($_POST['Las_Lenas,Argentina']) ? $destino_laslenas_argentina = 'NO' : $destino_laslenas_argentina = 'SI';
-      empty($_POST['Grandvalira,Andorra']) ? $destino_grandvalira_andorra = 'NO' : $destino_grandvalira_andorra = 'SI';
-      empty($_POST['Sierra_Nevada,España']) ? $destino_sierra_nevada = 'NO' : $destino_sierra_nevada = 'SI';
-      empty($_POST['otro_destino']) ? $destino_otro = 'NO' : $destino_otro = $_POST['otro_destino'];
-      $fecha_desde = $_POST['fecha_desde'];
-      $fecha_hasta = $_POST['fecha_hasta'];
-      empty($_POST['fechas_flexibles']) ? $fechas_flexibles = 'NO' : $fechas_flexibles = 'SI';
-      $cantidad_personas = $_POST['cantidad_personas'];
-      $cantidad_niños = $_POST['cantidad_niños'];
-      $edad_promedio_niños = $_POST['edad_promedio_niños'];
-      $preferencia_hotel = $_POST['preferencia_hotel'];
-      empty($_POST['alquilar_equipo']) ? $alquiler_equipo = 'NO' : $alquiler_equipo = 'SI';
-      empty($_POST['tomara_clases']) ? $tomar_clases = 'NO' : $tomar_clases = 'SI';
-      empty($_POST['Esquiar']) ? $objetivo_esquiar = 'NO' : $objetivo_esquiar = 'SI';
-      empty($_POST['Familia']) ? $objetivo_familia = 'NO' : $objetivo_familia = 'SI';
-      empty($_POST['Vida_Nocturna']) ? $objetivo_vida_nocturna = 'NO' : $objetivo_vida_nocturna = 'SI';
-      empty($_POST['Relax/Spa']) ? $objetivo_relax_spa = 'NO' : $objetivo_relax_spa = 'SI';
-      empty($_POST['Shopping']) ? $objetivo_shopping = 'NO' : $objetivo_shopping = 'SI';
-      empty($_POST['otro_objetivo']) ? $objetivo_otro = 'NO' : $objetivo_otro = $_POST['otro_objetivo'] ;
-      empty($_POST['observaciones']) ? $observaciones = 'NO' : $observaciones = $_POST['observaciones'];
-      $fecha = date("Y-m-d");
+  } elseif(isset($_POST["formulario"])) {
+    // Preparo variables
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $lugar_inicio_viaje = $_POST['inicio_viaje'];
+    empty($_POST['Chapelco,San_Martin_de_los_Andes']) ? $destino_chapelco = 'NO' : $destino_chapelco = 'SI';
+    empty($_POST['Aspen_Snowmas,Colorado']) ? $destino_aspen_colorado = 'NO' : $destino_aspen_colorado = 'SI';
+    empty($_POST['Baquera_Beret,España']) ? $destino_baqueria_espana = 'NO' : $destino_baqueria_espana = 'SI';
+    empty($_POST['Niseko,Japon']) ? $destino_niseko_japon = 'NO' : $destino_niseko_japon = 'SI';
+    empty($_POST['Las_Lenas,Argentina']) ? $destino_laslenas_argentina = 'NO' : $destino_laslenas_argentina = 'SI';
+    empty($_POST['Grandvalira,Andorra']) ? $destino_grandvalira_andorra = 'NO' : $destino_grandvalira_andorra = 'SI';
+    empty($_POST['Sierra_Nevada,España']) ? $destino_sierra_nevada = 'NO' : $destino_sierra_nevada = 'SI';
+    empty($_POST['otro_destino']) ? $destino_otro = 'NO' : $destino_otro = $_POST['otro_destino'];
+    $fecha_desde = $_POST['fecha_desde'];
+    $fecha_hasta = $_POST['fecha_hasta'];
+    empty($_POST['fechas_flexibles']) ? $fechas_flexibles = 'NO' : $fechas_flexibles = 'SI';
+    $cantidad_personas = $_POST['cantidad_personas'];
+    $cantidad_ninos = $_POST['cantidad_niños'];
+    $edad_promedio_ninos = $_POST['edad_promedio_niños'];
+    $preferencia_hotel = $_POST['preferencia_hotel'];
+    empty($_POST['alquilar_equipo']) ? $alquiler_equipo = 'NO' : $alquiler_equipo = 'SI';
+    empty($_POST['tomara_clases']) ? $tomar_clases = 'NO' : $tomar_clases = 'SI';
+    empty($_POST['Esquiar']) ? $objetivo_esquiar = 'NO' : $objetivo_esquiar = 'SI';
+    empty($_POST['Familia']) ? $objetivo_familia = 'NO' : $objetivo_familia = 'SI';
+    empty($_POST['Vida_Nocturna']) ? $objetivo_vida_nocturna = 'NO' : $objetivo_vida_nocturna = 'SI';
+    empty($_POST['Relax/Spa']) ? $objetivo_relax_spa = 'NO' : $objetivo_relax_spa = 'SI';
+    empty($_POST['Shopping']) ? $objetivo_shopping = 'NO' : $objetivo_shopping = 'SI';
+    empty($_POST['otro_objetivo']) ? $objetivo_otro = 'NO' : $objetivo_otro = $_POST['otro_objetivo'] ;
+    empty($_POST['observaciones']) ? $observaciones = 'NO' : $observaciones = $_POST['observaciones'];
+    $fecha_envio = date("Y-m-d");
 
-      // Guardo en DB
-      $tabla_db = "formulario";
-      $query_formulario= "INSERT INTO $tabla_db (`name`, `email`, `lugar_inicio_viaje`, `destino_chapelco`, `destino_aspen_colorado`, `destino_laslenas_argentina`, `destino_grandvalira_andorra`, `destino_baqueria_espana`, `destino_niseko_japon`,`destino_sierra_nevada`, `destino_otro`, `fecha_desde`, `fecha_hasta`,`fechas_flexibles`, `cantidad_personas`, `cantidad_ninos`, `edad_promedio_ninos`, `preferencia_hotel`, `alquiler_equipo`, `tomar_clases`, `objetivo_esquiar`, `objetivo_familia`, `objetivo_vida_nocturna`, `objetivo_relax_spa`, `objetivo_shopping`, `objetivo_otro`, `observaciones`, `fecha_envio`) VALUES ('$nombre', '$email', '$lugar_inicio_viaje',' $destino_chapelco', '$destino_aspen_colorado', '$destino_laslenas_argentina', '$destino_grandvalira_andorra', '$destino_baqueria_españa', '$destino_niseko_japon','$destino_sierra_nevada', '$destino_otro', '$fecha_desde', '$fecha_hasta', $fechas_flexibles, '$cantidad_personas', '$cantidad_niños', '$edad_promedio_niños', '$preferencia_hotel', '$alquiler_equipo', '$tomar_clases', '$objetivo_esquiar', '$objetivo_familia', '$objetivo_vida_nocturna', '$objetivo_relax_spa', '$objetivo_shopping', '$objetivo_otro', '$observaciones', '$fecha')";
-      mysqli_query($conexion_db, $query_formulario);
-      //Prepara archivo csv
-      require_once('mail/saveCSV.php');
-      //Prepara mail
-      $subject = "Encuesta desde Web";
-      $message = "<b>Nombre:</b> " . $nombre;
-      $message .= "<br><br><b>Email:</b> " . $email;
-      $message .= "<br><br><b>Lugar de inicio del viaje:</b> " . $lugar_inicio_viaje;
-      $message .= "<br><br><u>DESTINOS</u> ";
-      $message .= "<br><br><b>Aspen Colorado:</b> " . $destino_aspen_colorado;
-      $message .= "<br><br><b>Chapelco:</b> " . $destino_chapelco;
-      $message .= "<br><br><b>Las Leñas Argentina:</b> " . $destino_laslenas_argentina;
-      $message .= "<br><br><b>Grandvalira Andorra:</b> " . $destino_grandvalira_andorra;
-      $message .= "<br><br><b>Baqueria España:</b> " . $destino_baqueria_españa;
-      $message .= "<br><br><b>Sierra Nevada España:</b> " . $destino_sierra_nevada;
-      $message .= "<br><br><b>Niseko Japón:</b> " . $destino_niseko_japon;
-      $message .= "<br><br><b>Otro:</b> " . $destino_otro;
-      $message .= "<br><br>";
-      $message .= "<br><br><u>FECHAS</u> ";
-      $message .= "<br><br><b>Fecha desde:</b> " . $fecha_desde;
-      $message .= "<br><br><b>Fecha hasta:</b> " . $fecha_hasta;
-      $message .= "<br><br><b>Fechas flexibles:</b> " . $fechas_flexibles;
-      $message .= "<br><br>";
-      $message .= "<br><br><u>PERSONAS</u> ";
-      $message .= "<br><br><b>Cantidad de personas:</b> " . $cantidad_personas;
-      $message .= "<br><br><b>Cantidad de niños:</b> " . $cantidad_niños;
-      $message .= "<br><br><b>Edad promedio de los niños:</b> " . $edad_promedio_niños;
-      $message .= "<br><br><b>Preferencia para el hotel:</b> " . $preferencia_hotel;
-      $message .= "<br><br><b>¿Alquila equipo esqui?:</b> " . $alquiler_equipo;
-      $message .= "<br><br><b>¿Toma clases de esqui o snowboard?:</b> " . $tomar_clases;
-      $message .= "<br><br>";
-      $message .= "<br><br><u>OBJETIVO DEL VIAJE</u>";
-      $message .= "<br><br><b>Esquiar:</b> " . $objetivo_esquiar;
-      $message .= "<br><br><b>Vida Nocturna:</b> " . $objetivo_vida_nocturna;
-      $message .= "<br><br><b>Familia:</b> " . $objetivo_familia;
-      $message .= "<br><br><b>Relax/Spa:</b> " . $objetivo_relax_spa;
-      $message .= "<br><br><b>Shopping:</b> " . $objetivo_shopping;
-      $message .= "<br><br><b>Otro:</b> " . $objetivo_otro;
-      $message .= "<br><br>";
-      $message .= "<br><br><b>Observaciones:</b> " . $observaciones;
-      $message .= "<br><br>";
-      $message .= "<br><br><u>DESCARGAR ARCHIVO ENCUESTAS</u>";
+    $keys = [
+      'name',
+      'email',
+      'lugar_inicio_viaje',
+      'destino_chapelco',
+      'destino_aspen_colorado',
+      'destino_laslenas_argentina',
+      'destino_grandvalira_andorra',
+      'destino_baqueria_espana',
+      'destino_niseko_japon',
+      'destino_sierra_nevada',
+      'destino_otro',
+      'fecha_desde',
+      'fecha_hasta',
+      'fechas_flexibles',
+      'cantidad_personas',
+      'cantidad_ninos',
+      'edad_promedio_ninos',
+      'preferencia_hotel',
+      'alquiler_equipo',
+      'tomar_clases',
+      'objetivo_esquiar',
+      'objetivo_familia',
+      'objetivo_vida_nocturna',
+      'objetivo_relax_spa',
+      'objetivo_shopping',
+      'objetivo_otro',
+      'observaciones',
+      'fecha_envio'
+    ];
 
-      $headers = "MIME-Version: 1.0" . "\r\n";
-      $headers .= "Content-type: text/html; charset=utf-8" . "\r\n";
-      $headers .= "From: Formulario Web <web@skirience.com>". "\r\n";
-      $headers .= "Reply-To: ".$nombre." <".$email.">". "\r\n";
+    $fields_db = implode(',', $keys);
+    $prepared_values =  ":";
+    $prepared_values .= implode(', :', $keys);
+    $values = compact($keys);
 
+    // Guardo en DB
+    $tabla_db = "formulario";
+    $query_formulario= "INSERT INTO $tabla_db ($fields_db) VALUES ($prepared_values);";
+    try {
+      $sth = $conexion_db->prepare($query_formulario);
+      $sth->execute($values);
+    }catch (Exception $e){
+      echo "Error!: " . $e->getMessage() . "<br/>";
+      error_log($e->getMessage);
+      die();
+    }
+    //Prepara archivo csv
+    require_once('./saveCSV.php');
+    //Prepara mail
+    $subject = "Encuesta desde Web";
+    $message = "<b>Nombre:</b> " . $name;
+    $message .= "<br><br><b>Email:</b> " . $email;
+    $message .= "<br><br><b>Lugar de inicio del viaje:</b> " . $lugar_inicio_viaje;
+    $message .= "<br><br><u>DESTINOS</u> ";
+    $message .= "<br><br><b>Aspen Colorado:</b> " . $destino_aspen_colorado;
+    $message .= "<br><br><b>Chapelco:</b> " . $destino_chapelco;
+    $message .= "<br><br><b>Las Leñas Argentina:</b> " . $destino_laslenas_argentina;
+    $message .= "<br><br><b>Grandvalira Andorra:</b> " . $destino_grandvalira_andorra;
+    $message .= "<br><br><b>Baqueria España:</b> " . $destino_baqueria_espana;
+    $message .= "<br><br><b>Sierra Nevada España:</b> " . $destino_sierra_nevada;
+    $message .= "<br><br><b>Niseko Japón:</b> " . $destino_niseko_japon;
+    $message .= "<br><br><b>Otro:</b> " . $destino_otro;
+    $message .= "<br><br>";
+    $message .= "<br><br><u>FECHAS</u> ";
+    $message .= "<br><br><b>Fecha desde:</b> " . $fecha_desde;
+    $message .= "<br><br><b>Fecha hasta:</b> " . $fecha_hasta;
+    $message .= "<br><br><b>Fechas flexibles:</b> " . $fechas_flexibles;
+    $message .= "<br><br>";
+    $message .= "<br><br><u>PERSONAS</u> ";
+    $message .= "<br><br><b>Cantidad de personas:</b> " . $cantidad_personas;
+    $message .= "<br><br><b>Cantidad de niños:</b> " . $cantidad_ninos;
+    $message .= "<br><br><b>Edad promedio de los niños:</b> " . $edad_promedio_ninos;
+    $message .= "<br><br><b>Preferencia para el hotel:</b> " . $preferencia_hotel;
+    $message .= "<br><br><b>¿Alquila equipo esqui?:</b> " . $alquiler_equipo;
+    $message .= "<br><br><b>¿Toma clases de esqui o snowboard?:</b> " . $tomar_clases;
+    $message .= "<br><br>";
+    $message .= "<br><br><u>OBJETIVO DEL VIAJE</u>";
+    $message .= "<br><br><b>Esquiar:</b> " . $objetivo_esquiar;
+    $message .= "<br><br><b>Vida Nocturna:</b> " . $objetivo_vida_nocturna;
+    $message .= "<br><br><b>Familia:</b> " . $objetivo_familia;
+    $message .= "<br><br><b>Relax/Spa:</b> " . $objetivo_relax_spa;
+    $message .= "<br><br><b>Shopping:</b> " . $objetivo_shopping;
+    $message .= "<br><br><b>Otro:</b> " . $objetivo_otro;
+    $message .= "<br><br>";
+    $message .= "<br><br><b>Observaciones:</b> " . $observaciones;
+    $message .= "<br><br>";
+    $message .= "<br><br><u>DESCARGAR ARCHIVO ENCUESTAS</u>";
 
-      // echo $nombre, $email, $lugar_inicio_viaje, $destino_aspen_colorado, $destino_laslenas_argentina, $destino_grandvalira_andorra, $destino_baqueria_españa, $destino_niseko_japon, $destino_otro, $fecha_aprox_inicio, $cantidad_dias, $fecha_desde, $fecha_hasta, $fechas_flexibles, $cantidad_personas, $cantidad_niños, $edad_promedio_niños, $preferencia_hotel, $alquiler_equipo, $tomar_clases, $objetivo_esquiar, $objetivo_familia, $objetivo_vida_nocturna, $objetivo_relax_spa, $objetivo_shopping, $objetivo_otro, $observaciones, $fecha; die();
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type: text/html; charset=utf-8" . "\r\n";
+    $headers .= "From: Formulario Web <web@skirience.com>". "\r\n";
+    $headers .= "Reply-To: ".$name." <".$email.">". "\r\n";
+
+    // echo $name, $email, $lugar_inicio_viaje, $destino_aspen_colorado, $destino_laslenas_argentina, $destino_grandvalira_andorra, $destino_baqueria_españa, $destino_niseko_japon, $destino_otro, $fecha_aprox_inicio, $cantidad_dias, $fecha_desde, $fecha_hasta, $fechas_flexibles, $cantidad_personas, $cantidad_niños, $edad_promedio_niños, $preferencia_hotel, $alquiler_equipo, $tomar_clases, $objetivo_esquiar, $objetivo_familia, $objetivo_vida_nocturna, $objetivo_relax_spa, $objetivo_shopping, $objetivo_otro, $observaciones, $fecha; die();
   }
-  // $error = mysqli_error($conexion_db);
-  // echo $error;
 
   // Cierra conexión DB
-  mysqli_close($conexion_db);
+  $conexion_db = null;
 
   //Envía Mail
   if( mail($to, $subject, $message, $headers) ) {
-  	echo "1";
+    echo "1";
   } else {
-  	echo "Error al enviar mensaje, intente nuevamente más tarde.";
+    echo "Error al enviar mensaje, intente nuevamente más tarde.";
   }
 
- ?>
+?>
